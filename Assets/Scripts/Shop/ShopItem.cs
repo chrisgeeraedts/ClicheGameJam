655a39fdaebf4bedead2aa6b9c897c9e3bccead6
NjitRam <njitram@gmail.com>
2022-11-13 23:46:40 +0100

    Shop scene expansions

---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Assets.Scripts.Shop
+{
+    public class ShopItem : MonoBehaviour
+    {
+        [SerializeField] ShopItemSO shopItemSo;
+        [SerializeField] SpriteRenderer itemIcon;
+        [SerializeField] TextMeshProUGUI priceText;
+        [SerializeField] Sprite soldOutSprite;
+
+
+        private void Start()
+        {
+            itemIcon.sprite = shopItemSo.ItemIcon;
+            priceText.text = shopItemSo.Price.ToString();
+        }
+
+        void OnClick()
+        { 
+            //Add button component
+            //Buy
+        }
+    }
+}
