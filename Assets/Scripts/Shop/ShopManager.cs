12398f8612d3ca27da817a6b1f9e6a5cbd53eed0
NjitRam <njitram@gmail.com>
2022-11-16 13:54:36 +0100

    Fix rotation of falling platform, add fishing pole to shop

---
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+using Assets.Scripts.Map;
+
+namespace Assets.Scripts.Shop
+{
+    public class ShopManager : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI coinsText;
+
+        private void Start()
+        {
+            UpdateCoinsText(MapManager.GetInstance().Coins);
+        }
+        public void UpdateCoinsText(int numberOfCoins)
+        {
+            coinsText.text = $"Coins: {numberOfCoins}";
+        }
+    }
+}
